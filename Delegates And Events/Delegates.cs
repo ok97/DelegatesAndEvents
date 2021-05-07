f7b231d8c1ac3654c0f1250d499dae252e94f712
ok97 <omkhawshi0@gmail.com>
2021-05-07 21:34:51 +0530

    Added: Single Delegates Function

---
+++ Delegates And Events/Delegates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_And_Events
+{ /* Single Delegates Function*/
+    public delegate int MyDelegates(int x, int y); // Define Delegates
+    class Delegates
+    {
+        public static int Addition(int x, int y) //create method that passes as parameter it has same signature as Delegates
+        {
+            return x + y;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+        static void Main(string[] args)
+        {
+            //MyDelegates myDelegates = new MyDelegates(Addition);//create object of Delegate Name
+            MyDelegates myDelegates = Addition; //create object of Delegate Name and Specify method Name
+            Console.WriteLine($"Addition is:- {Addition(55, 5)}"); //Passing parameter and Print
+            Console.WriteLine($"Multiplication is:- {Multiply(5, 5)}");
+            Console.ReadLine();
+        }
+    }
+}
